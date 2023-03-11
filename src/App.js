90e5e48b1715190d1fd6b3652fd6434ba05bfd29
sachinsisania <sachinsisania@gmail.com>
2023-03-11 17:43:58 +0530

    add footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Searchbar from "./components/Searchbar";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar title="GET SRC" />
       <Searchbar />
+      <Footer />
     </>
   );
 }
